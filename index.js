e6a9ef11be0908993c006e2d9d706420f049e81c
Wen Li <i1s2b@ugrad.cs.ubc.ca>
2021-04-21 19:09:03 -0700

    fixed some logic

--- index.js
+++ index.js
@@ -25,23 +25,35 @@ client.on('message', async msg => {
     console.log("Arguments: ", args)
 
     if (message.trim() === "!help") {
-        msg.reply(helpCommand());
-    } else if (message.startsWith("!commands")) {
+        msg.channel.send(helpCommand()).then(
+            () => console.log('!help success')
+        ).catch(console.error);
+    } else if (message === "!commands") {
         msg.reply("d");
-    } else if (message.startsWith("!addcommand")) {
+    } else if (args[0] === "!addcommand") {
         msg.reply("successfully added [command]");
         console.log("[user] added [command]")
-    } else if (message.startsWith("!deletecommand")) {
+    } else if (args[0] === "!deletecommand") {
         msg.reply("successfully deleted [command]");
         console.log("[user] deleted [command]")
-    } else if (message.startsWith("!resetcommand")) {
+    } else if (args[0] === "!resetcommand") {
         msg.reply("YOU THOUGHT LOL!");
-    } else if (message.startsWith("!reset")) {
-        msg.reply("d");
+    } else if (args[0] === "!reset") {
+        if (args[1] !== password) {
+            msg.reply("Wrong password.");
+        } else {
+            console.log("dumped db")
+        }
     } else if (message.trim() === "!secret") {
-        msg.reply(secretCommand());
+        msg.channel.send(secretCommand()).then(
+            () => console.log('!secret success')
+        ).catch(console.error);
     } else if (message.trim() === "!roll") {
         msg.reply(roll());
+    } else if (message.trim().includes("joe")) {
+        msg.channel.send("mama").then(
+            () => console.log('joe success')
+        ).catch(console.error);
     } else if (message.startsWith("!")) {
         msg.reply("Command not found, check !help.");
     }
