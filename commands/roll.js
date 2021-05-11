84a852f405ec643ebfe2f7af453f9e8a5c331ba4
Wen Li <i1s2b@ugrad.cs.ubc.ca>
2021-05-10 17:04:33 -0700

    fixed bounds

--- commands/roll.js
+++ commands/roll.js
@@ -2,14 +2,14 @@ module.exports = {
     roll: () => {
         const roll = Math.floor(Math.random() * 100) + 1;
         const hundo = " POGGERS!";
-        const zero = " LUUUUUUUUUUUUUUUUUUUUUUUUL!";
+        const one = " LUUUUUUUUUUUUUUUUUUUUUUUUL!";
         let result = `You rolled a ${roll}!`;
 
         if (roll === 100) {
             result += hundo;
         }
-        if (roll === 0) {
-            result += zero;
+        if (roll === 1) {
+            result += one;
         }
         return result;
     }
